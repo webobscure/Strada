dfcb9898e24315acf11eef7215352ac5a4ed3687
webobscure <sparkygino@gmail.com>
2022-08-14 21:14:56 +0300

    Ready to code review

---
+++ calculate-compare/webobscure/calculator.js
@@ -0,0 +1,22 @@
+function calc( operator, a , b ) {
+    switch(operator) {
+        case 'add' :
+          return  a + b;
+        
+        case 'substract' :
+            return  a - b;
+        
+        case 'divide' :
+            return parseInt(a / b);
+        
+        case 'multiple' :
+            return a * b;
+
+        default: return console.log('Choose the operation');
+    }
+}
+
+console.log(calc('add', 5, 3));
+console.log(calc('substract', 200, 245));
+console.log(calc('divide', 50, 7));
+console.log(calc('multiple', 5, 10))
