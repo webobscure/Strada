2bcbe1d523e777a178a17c0ad5acadedc0c097e2
webobscure <sparkygino@gmail.com>
2022-08-15 19:39:03 +0300

    change checks

--- calculator.js
+++ calculator.js
@@ -1,22 +1,34 @@
 function calc( operator, a , b ) {
-    switch(operator) {
-        case 'add' :
-          return  a + b;
-        
-        case 'substract' :
-            return  a - b;
-        
-        case 'divide' :
-            return parseInt(a / b);
-        
-        case 'multiple' :
-            return a * b;
+    if (checkOperands(a) && checkOperands(b)) {
+        switch(operator) {
+            case 'add' :
+              return  a + b;
+            
+            case 'substract' :
+                return  a - b;
+            
+            case 'divide' :
+                return parseInt(a / b);
+            
+            case 'multiple' :
+                return a * b;
+    
+            case 'pow' : 
+                return a ** b;
+    
+            default: return console.log('Choose the operation!');
+        }
+    } 
 
-        default: return console.log('Choose the operation!');
-    }
+    return 'Incorrect operands'
+}
+
+function checkOperands(operand) {
+    return typeof operand === 'number'
 }
 
 console.log(calc('add', 5, 3));
 console.log(calc('substract', 200, 245));
 console.log(calc('divide', 50, 7));
 console.log(calc('multiple', 5, 10))
+console.log(calc('pow', 2, 10));
---
+++ cycle.js
@@ -0,0 +1,21 @@
+let i = 1
+    // цикл while с постусловием
+    do {
+        console.log(i++);
+    } while (i <= 19)
+
+    // цикл for
+
+    for (; i<= 19; i++) {
+        console.log(i);
+    }
+
+
+    // цикл while с предусловием
+
+    while (i <= 19) {
+        console.log(i++);
+    }
+
+
+
