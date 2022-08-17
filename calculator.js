7e197299f703367b30d1eb0f098813a9857a0d3a
webobscure <sparkygino@gmail.com>
2022-08-17 19:24:08 +0300

    add object

--- calculator.js
+++ calculator.js
@@ -1,19 +1,27 @@
+let operations = {
+    add: 'add',
+    substract: 'substract',
+    divide: 'divide',
+    multiple: 'multiple',
+    pow: 'pow'
+}
+
 function calc( operator, a , b ) {
     if (checkOperands(a) && checkOperands(b)) {
         switch(operator) {
-            case 'add' :
+            case operations.add :
               return  a + b;
             
-            case 'substract' :
+            case operations.substract :
                 return  a - b;
             
-            case 'divide' :
+            case operations.divide :
                 return parseInt(a / b);
             
-            case 'multiple' :
+            case operations.multiple :
                 return a * b;
     
-            case 'pow' : 
+            case operations.pow : 
                 return a ** b;
     
             default: return console.log('Choose the operation!');
@@ -27,8 +35,8 @@ function checkOperands(operand) {
     return typeof operand === 'number'
 }
 
-console.log(calc('add', 5, 3));
-console.log(calc('substract', 200, 245));
-console.log(calc('divide', 50, 7));
-console.log(calc('multiple', 5, 10))
-console.log(calc('pow', 2, 10));
+console.log(calc(operations.add, 5, 3));
+console.log(calc(operations.substract, 200, 245));
+console.log(calc(operations.divide, 50, 7));
+console.log(calc(operations.multiple, 5, 10))
+console.log(calc(operations.multiple, 2, 10));
